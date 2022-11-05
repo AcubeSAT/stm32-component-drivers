cdcaeaf9baf5e829f2e9f7613a28fcf13f9cf490
Stelios Tzelepis <stel.tze09@gmail.com>
2022-11-05 13:15:05 +0200

    Define PIOs from manufacturer

--- LCL/inc/LCL.hpp
+++ LCL/inc/LCL.hpp
@@ -16,9 +16,19 @@ class LCL {
 private:
 
     /**
-     * PWM channels used to set the current threshold for the TLC555 timer.
+     * @enum Devices protected by LCLs
      */
-    enum PWMChannel : uint8_t {
+    enum class ProtectedDevices : uint8_t {
+        NANDFlash = 0,
+        MRAM = 1,
+        CAN1 = 2,
+        CAN2 = 3
+    };
+
+    /**
+     * @enum PWM channels used to set the current threshold for the TLC555 timer.
+     */
+    enum class PWMChannel : uint8_t {
         PWMC0_PWMH0 = 0, ///< NAND Flash
         PWM0_PWMH1 = 1, ///< MRAM
         PWMC0_PWMH3 = 2, ///< CAN1
@@ -26,39 +36,45 @@ private:
     };
 
     /**
-     * GPIOs used reset the LCL, specifically the TLC555 IC.
+     * @enum GPIOs used reset the LCL, specifically the TLC555 IC.
      * Active low.
      * Initial value given by a pull-up resistor to prevent undefined behavior during MCU
      * reset/start-up.
      */
-    enum ResetPins : uint8_t {
-        PC15 = 0, ///< NAND Flash
-        PE4 = 1, ///< MRAM
-        PD24 = 2, ///< CAN1
-        PA26 = 3 ///< CAN2
+    enum class ResetPins : uint8_t {
+        MRAM = PIO_PIN_PC15, ///< NAND Flash
+        NANDFlash = PIO_PIN_PE4, ///< MRAM
+        CAN1 = PIO_PIN_PD24, ///< CAN1
+        CAN2 = PIO_PIN_PA26 ///< CAN2
     };
 
     /**
-     * GPIOs used reset the LCL, specifically the TLC555 IC.
+     * @enum GPIOs used reset the LCL, specifically the TLC555 IC.
      * Active low.
      * Initial value given by a pull-up resistor to prevent undefined behavior during MCU
      * reset/start-up.
      */
-    enum SetPins : uint8_t {
-        PC13 = 0, ///< MRAM
-        PA17 = 1, ///< NAND Flash
-        PD26 = 2, ///< CAN1
-        PD22 = 3 ///< CAN2
+    enum class SetPins : uint8_t {
+        MRAM = PIO_PIN_PC13, ///< MRAM
+        NANDFlash = PIO_PIN_PA17, ///< NAND Flash
+        CAN1 = PIO_PIN_PD26, ///< CAN1
+        CAN2 = PIO_PIN_PD22 ///< CAN2
     };
 
 public:
 
     LCL();
+
     void returnLCLStatus();
+
     void openLCL();
+
     void closeLCL();
+
     void calculateVoltageThreshold();
+
     void calculateCurrentThreshold();
+
     void changeCurrentThreshHold();
 };
 
--- LCL/src/LCL.cpp
+++ LCL/src/LCL.cpp
@@ -1,7 +1,12 @@
 #include "LCL.hpp"
 
+LCL::LCL() {
+
+}
+
 void LCL::openLCL() {
-    PWM0_ChannelsStart(PWM_CHANNEL_0_MASK);
+    PWM0_ChannelsStart(PWM_CHANNEL_0_MASK); PWM0_ChannelsStop() PWM0_ChannelPeriodGet
+    (PWM_CHANNEL_0) PWM0_ChannelDutySet(PWM_CHANNEL_0, duty0);
     PIO_PinWrite(PIO_PIN_PB2, true); // RESET
     PIO_PinWrite(PIO_PIN_PB3, true); // SET/TRIG
     SYSTICK_DelayMs(500);
