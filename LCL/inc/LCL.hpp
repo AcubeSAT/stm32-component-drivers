17287009c467575304e890b3459c55d0502a6985
Stelios Tzelepis <stel.tze09@gmail.com>
2022-11-08 18:08:50 +0200

    Set up LCL header

--- LCL/inc/LCL.hpp
+++ LCL/inc/LCL.hpp
@@ -1,76 +1,13 @@
 #pragma once
 
-#ifndef ATSAM_COMPONENT_DRIVERS_LCL_HPP
-#define ATSAM_COMPONENT_DRIVERS_LCL_HPP
+#include "LCLDefinitions.hpp"
 
 namespace LCLDefinitions {
 
     /**
      *
      */
-    struct LCLControlPins {
-        ResetPin resetPin = 0;
-        SetPins setPin = 0;
-        PWMChannelMasks pwmChannelMask = 0;
-        PWMChannelNumbers pwmChannelNumber = 0;
-    };
-
-    /**
-     * @enum Devices protected by LCLs
-     */
-    enum class ProtectedDevices : uint8_t {
-        NANDFlash = 0,
-        MRAM = 1,
-        CAN1 = 2,
-        CAN2 = 3
-    };
-
-    /**
-     * @enum PWM channels used to set the current threshold for the TLC555 timer.
-     * PWM0 of ATSAMV71 is used for all thresholds.
-     */
-    enum class PWMChannelMasks : uint8_t {
-        NANDFlash = PWM_CHANNEL_0_MASK, ///< NAND Flash
-        MRAM = PWM_CHANNEL_1_MASK, ///< MRAM
-        CAN1 = PWM_CHANNEL_3_MASK, ///< CAN1
-        CAN2 = PWM_CHANNEL_2_MASK ///< CAN2
-    };
-
-    /**
-     * @enum PWM channels used to set the current threshold for the TLC555 timer.
-     */
-    enum class PWMChannelNumbers : uint8_t {
-        NANDFlash = PWM_CHANNEL_0, ///< NAND Flash
-        MRAM = PWM_CHANNEL_1, ///< MRAM
-        CAN1 = PWM_CHANNEL_3, ///< CAN1
-        CAN2 = PWM_CHANNEL_2 ///< CAN2
-    };
-
-    /**
-     * @enum GPIOs used reset the LCL, specifically the TLC555 IC.
-     * Active low.
-     * Initial value given by a pull-up resistor to prevent undefined behavior during MCU
-     * reset/start-up.
-     */
-    enum class ResetPins : uint8_t {
-        NANDFlash = PIO_PIN_PC15, ///< NAND Flash
-        MRAM = PIO_PIN_PE4, ///< MRAM
-        CAN1 = PIO_PIN_PD24, ///< CAN1
-        CAN2 = PIO_PIN_PA26 ///< CAN2
-    };
-
-    /**
-     * @enum GPIOs used reset the LCL, specifically the TLC555 IC.
-     * Active low.
-     * Initial value given by a pull-up resistor to prevent undefined behavior during MCU
-     * reset/start-up.
-     */
-    enum class SetPins : uint8_t {
-        NANDFlash = PIO_PIN_PC13, ///< MRAM
-        MRAM = PIO_PIN_PA17, ///< NAND Flash
-        CAN1 = PIO_PIN_PD26, ///< CAN1
-        CAN2 = PIO_PIN_PD22 ///< CAN2
-    };
+    struct LCLControlPins;
 }
 
 /**
@@ -84,14 +21,16 @@ namespace LCLDefinitions {
 class LCL {
 private:
 
-    LCLDefinitions::LCLControlPins controlPins = {0};
+    LCLDefinitions::LCLControlPins& controlPins;
 
-    inline uint16_t pwmDutyCycle = 0;
+    uint16_t pwmDutyCycle = 0;
 
     // inline float currentThreshold = 0;
 public:
 
-    void returnLCLStatus(LCLDefinitions::LCLControlPins) : ;
+    LCL(LCLDefinitions::LCLControlPins& controlPinsStruct) : controlPins(controlPinsStruct) {}
+
+    void returnLCLStatus();
 
     void openLCL();
 
@@ -103,5 +42,3 @@ public:
 
     void changeCurrentThreshHold();
 };
-
-#endif //ATSAM_COMPONENT_DRIVERS_LCL_HPP
--- LCL/src/LCL.cpp
+++ LCL/src/LCL.cpp
@@ -1,43 +1,43 @@
-#include "LCL.hpp"
-
-LCL::LCL() {
+//#include "LCL.hpp"
+//
+//LCL::LCL() {
+////    PIO_PinWrite(PIO_PIN_PB2, true); // RESET
+////    PIO_PinWrite(PIO_PIN_PB3, true); // SET/TRIG
+//}
+//
+//void LCL::returnLCLStatus() {
+//    PWM0_ChannelPeriodGet();
+//    calculateCurrentThreshold();
+//
+//    return struct
+//}
+//
+//void LCL::openLCL() {
+//    // Todo assert status of GPIOs/log
 //    PIO_PinWrite(PIO_PIN_PB2, true); // RESET
 //    PIO_PinWrite(PIO_PIN_PB3, true); // SET/TRIG
-}
-
-void LCL::returnLCLStatus() {
-    PWM0_ChannelPeriodGet();
-    calculateCurrentThreshold();
-
-    return struct
-}
-
-void LCL::openLCL() {
-    // Todo assert status of GPIOs/log
-    PIO_PinWrite(PIO_PIN_PB2, true); // RESET
-    PIO_PinWrite(PIO_PIN_PB3, true); // SET/TRIG
-    PWM0_ChannelsStart(PWM_CHANNEL_0_MASK);
-
-    PIO_PinWrite(PIO_PIN_PB3, false); // enable set
-    SYSTICK_DelayMs(100);
-    PIO_PinWrite(PIO_PIN_PB3, true); // disable set
-}
-
-void LCL::closeLCL() {
-    // Todo assert status of GPIOs/log
-    PWM0_ChannelsStop();
-    PIO_PinWrite(PIO_PIN_PB2, false); // enable RESET
-}
-
-void LCL::calculateVoltageThreshold() {
-
-}
-
-void LCL::calculateCurrentThreshold() {
-
-}
-
-void LCL::changeCurrentThreshHold() {
-    PWM0_ChannelDutySet(PWM_CHANNEL_0, duty0);
-
-}
+//    PWM0_ChannelsStart(PWM_CHANNEL_0_MASK);
+//
+//    PIO_PinWrite(PIO_PIN_PB3, false); // enable set
+//    SYSTICK_DelayMs(100);
+//    PIO_PinWrite(PIO_PIN_PB3, true); // disable set
+//}
+//
+//void LCL::closeLCL() {
+//    // Todo assert status of GPIOs/log
+//    PWM0_ChannelsStop();
+//    PIO_PinWrite(PIO_PIN_PB2, false); // enable RESET
+//}
+//
+//void LCL::calculateVoltageThreshold() {
+//
+//}
+//
+//void LCL::calculateCurrentThreshold() {
+//
+//}
+//
+//void LCL::changeCurrentThreshHold() {
+//    PWM0_ChannelDutySet(PWM_CHANNEL_0, duty0);
+//
+//}
