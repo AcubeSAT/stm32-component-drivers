2c51f2b422d6bf943b802d0426a6b642471731d2
JulioLu <lulixhulio@gmail.com>
2022-11-20 20:29:05 +0200

    add basic read-write commands

--- NANDFlash/Inc/NANDFlash.h
+++ NANDFlash/Inc/NANDFlash.h
@@ -14,68 +14,9 @@ private:
     static uint32_t addressRegister = 0x60200000;
     static uint32_t commandRegister = 0x60400000;
 
-    void writeData(uint8_t data) {
-        *((volatile uint8_t *) dataRegister) = (uint8_t) data;
-    }
+    void writeData(uint8_t data);
 
-    void sendAddress(uint8_t address) {
-        *((volatile uint8_t *) addressRegister) = (uint8_t) address;
-    }
+    void sendAddress(uint8_t address);
 
-    void sendCommand(uint8_t command) {
-        *((volatile uint8_t *) commandRegister) = (uint8_t) command;
-    }
-
-    PIO_PIN NANDOE = PIO_PIN_PC9;
-    PIO_PIN NANDWE = PIO_PIN_PC10;
-    PIO_PIN NANDCLE  = PIO_PIN_PC17;
-    PIO_PIN NANDALE = PIO_PIN_PC16;
-    PIO_PIN NCS = PIO_PIN_PA3;
-public:
-
-    void enableCS() {
-        PIO_PinWrite(NCS, false);
-    }
-
-    void disableCS() {
-        PIO_PinWrite(NCS, true);
-    }
-
-    void busIdle() {
-        PIO_PinWrite(NCS, false);
-        PIO_PinWrite(NANDWE, true);
-        PIO_PinWrite(NANDOE, true);
-    }
-
-    void sendCommandToNAND(uint8_t command) {
-        sendCommand(command);
-        PIO_PinWrite(NCS, false);
-        PIO_PinWrite(NANDALE, false);
-        PIO_PinWrite(NANDCLE, true);
-        PIO_PinWrite(NANDOE, true);
-    }
-
-    void sendAddressToNAND(uint8_t address) {
-        sendAddress(address);
-        PIO_PinWrite(NCS, true);
-        PIO_PinWrite(NANDALE, true);
-        PIO_PinWrite(NANDCLE, false);
-        PIO_PinWrite(NANDOE, true);
-    }
-
-    void writeDataToNAND(uint8_t data) {
-        WriteData(data);
-        PIO_PinWrite(NCS, false);
-        PIO_PinWrite(NANDALE, false);
-        PIO_PinWrite(NANDCLE, false);
-        PIO_PinWrite(NANDOE, true);
-    }
-
-    uint8_t readDataFromNAND() {
-        PIO_PinWrite(NCS, false);
-        PIO_PinWrite(NANDALE, false);
-        PIO_PinWrite(NANDCLE, false);
-        PIO_PinWrite(NANDWE, false);
-        return *((volatile uint8_t *) dataRegister) = (uint8_t) data;
-    }
+    void sendCommand(uint8_t command);
 };
--- NANDFlash/Src/NANDFlash.cpp
+++ NANDFlash/Src/NANDFlash.cpp
@@ -1,4 +1,17 @@
 #include "NANDFlash.h"
 
-NANDFlash::NANDFlash(NAND_HandleTypeDef *nand) : nand(nand) {}
+
+    void MT29F::writeData(uint8_t data) {
+        *((volatile uint8_t *) dataRegister) = (uint8_t) data;
+    }
+
+    void MT29F::sendAddress(uint8_t address) {
+        *((volatile uint8_t *) addressRegister) = (uint8_t) address;
+    }
+
+    void MT29F::sendCommand(uint8_t command) {
+        *((volatile uint8_t *) commandRegister) = (uint8_t) command;
+    }
+
+};
 
