a80695b8e29e9667f06f2264fbb70cafe636bd6a
kchristin22 <christinakoutsou22@gmail.com>
2022-12-02 14:08:11 +0200

    Move function implementation to header file

--- NANDFlash/Inc/NANDFlash.h
+++ NANDFlash/Inc/NANDFlash.h
@@ -1,7 +1,7 @@
 #include <cstdint>
 #include "definitions.h"
 
-
+#pragma once
 /**
  * This is a driver for MT29F NAND Flash.
  * @ingroup drivers
@@ -21,13 +21,41 @@ private:
     PIO_PIN NCS = PIO_PIN_PD19;
 
 public:
-    uint8_t initialize();
 
     void writeData(uint8_t data);
 
     void sendAddress(uint8_t address);
 
-    void sendCommand(uint8_t command);
+    void sendCommand(uint8_t command){
+        PIO_PinWrite(NCS, false);
+        PIO_PinWrite(NANDALE, false);
+        PIO_PinWrite(NANDOE, true);
+        PIO_PinWrite(NANDCLE, false);
+        *((volatile uint8_t *) commandRegister) = (uint8_t) command;
+        PIO_PinWrite(NCS, true);
+    }
 
     uint8_t readDataFromNAND();
+
+    uint8_t initialize(){    MATRIX_REGS->CCFG_SMCNFCS = 8;
+        uint8_t data;
+        while(1) {
+            if (PIO_PinRead(PIO_PIN_PA12)) {
+                sendCommand(0xFF);
+                while (1) {
+                    if (PIO_PinRead(PIO_PIN_PA12)) {
+                        sendCommand(0x70);
+                        break;
+                    }
+                }
+                PIO_PinWrite(NCS, false);
+                PIO_PinWrite(NANDOE, false);
+                for(int i = 0; i < 10; i++){
+                    data = *((volatile uint8_t *) dataRegister);
+                }
+                break;
+            }
+        }
+        return data;
+    }
 };
