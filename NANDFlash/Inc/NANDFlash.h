fcde7dff67ac13b291d3275092fff0cbd7069c7c
kchristin22 <christinakoutsou22@gmail.com>
2022-12-02 23:04:17 +0200

    Move functions' implementation to cpp file

--- NANDFlash/Inc/NANDFlash.h
+++ NANDFlash/Inc/NANDFlash.h
@@ -22,40 +22,14 @@ private:
 
 public:
 
+    uint8_t initialize();
+
     void writeData(uint8_t data);
 
     void sendAddress(uint8_t address);
 
-    void sendCommand(uint8_t command){
-        PIO_PinWrite(NCS, false);
-        PIO_PinWrite(NANDALE, false);
-        PIO_PinWrite(NANDOE, true);
-        PIO_PinWrite(NANDCLE, false);
-        *((volatile uint8_t *) commandRegister) = (uint8_t) command;
-        PIO_PinWrite(NCS, true);
-        PIO_PinWrite(NANDOE, false);
-    }
+    void sendCommand(uint8_t command);
 
     uint8_t readDataFromNAND();
 
-    uint8_t initialize(){
-        uint8_t data;
-        while(1) {
-            if (PIO_PinRead(PIO_PIN_PA12)) {
-                sendCommand(0xFF);
-                while (1) {
-                    if (PIO_PinRead(PIO_PIN_PA12)) {
-                        sendCommand(0x70);
-                        break;
-                    }
-                }
-                PIO_PinWrite(NCS, false);
-                for(int i = 0; i < 10; i++){
-                    data = *((volatile uint8_t *) dataRegister);
-                }
-                break;
-            }
-        }
-        return data;
-    }
 };
