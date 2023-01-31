c48845d40bb52f38b70e196057d8d31cb59df285
Stelios Tzelepis <stel.tze09@gmail.com>
2023-02-01 00:03:21 +0200

    Add documentation

--- MRAM/inc/MR4A08BUYS45.hpp
+++ MRAM/inc/MR4A08BUYS45.hpp
@@ -5,38 +5,44 @@
 class MRAM : public SMC {
 private:
     /**
-     *
+     * The MRAM has a total of 21 address pins, organised as 2,097,152 (2^21) words of 8 bits.
+     * The device can store data from address 0 to 2^21-1.
+     * Primarily used for error detection.
      */
     static static constexpr uint32_t memoryAddressLimit = 0x1FFFFF;
 
-    /*
-     *
+    /**
+     * A variable to keep track of the total allocated memory and help in inserting new data.
      */
     uint32_t nextUnallocatedMemoryAddress = 0;
+
+//     WIP: a map holding the first address (and maybe the total size) specific parameters are
+//     allocated (and allocate).
+//    etl::map<uint8_t, etl::pair<uint32_t, uint32_t>, 10> memoryMapping;
 public:
     /**
-     *
-     * @param chipSelect
+     * Utilizes the \ref SMC constructor to initialize the \ref moduleBaseAddress constant.
+     * @param chipSelect Number of the Chip Select used for enabling the MRAM.
      */
     constexpr MRAM(ChipSelect chipSelect) : SMC(chipSelect) {}
 
     /**
-     *
-     * @param dataAddress
-     * @param data
+     * Basic 8-bit write that takes advantage of the \ref moduleBaseAddress and creates an abstraction
+     * to receive as a parameter directly an MRAM address.
+     * @param dataAddress MRAM address to write to.
+     * @param data 8-bit data to write to the address.
      */
     inline void mramDataWrite(uint32_t dataAddress, uint8_t data) {
         smcDataWrite(moduleBaseAddress | dataAddress, data);
     }
 
     /**
-     *
-     * @param dataAddress
-     * @return
+     * Basic 8-bit write that takes advantage of the \ref moduleBaseAddress and creates an abstraction
+     * to receive as a parameter directly an MRAM address.
+     * @param dataAddress MRAM address to write to.
+     * @return 8-bit data saved in that address.
      */
     inline uint8_t mramDataRead(uint32_t dataAddress) {
         return smcDataRead(moduleBaseAddress | dataAddress);
     }
 };
-
-//inline std::optional<MRAM> mram;
