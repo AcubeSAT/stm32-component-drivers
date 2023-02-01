6d317862ce49935c46397c6b56133d4ccc35135e
Stelios Tzelepis <stel.tze09@gmail.com>
2023-02-01 23:39:16 +0200

    Add read-busy pin

--- NANDFlash/inc/NANDFlash.h
+++ NANDFlash/inc/NANDFlash.h
@@ -13,22 +13,23 @@
  *  and https://gr.mouser.com/datasheet/2/671/micron_technology_micts05995-1-1759202.pdf
  */
 class MT29F : public SMC {
+public:
+    // WIP enum
+//    enum NandReadyBusyPin : PIO_PIN {
+//        MemoryPartition1 = MEM_NAND_BUSY_1_PIN,
+//        MemoryPartition2 = MEM_NAND_BUSY_2_PIN
+//    };
+
 private:
     const uint32_t triggerNANDALEAddress = moduleBaseAddress | 0x200000;
     const uint32_t triggerNANDCLEAddress = moduleBaseAddress | 0x400000;
 
-    PIO_PIN nandReadyBusyPin = static_cast<PIO_PIN>(0);
+    const PIO_PIN nandReadyBusyPin = PIO_PIN_NONE; //TODO: if PIO_PIN == NONE => throw error
 //    PIO_PIN NANDOE = PIO_PIN_PC9;
 //    PIO_PIN NANDWE = PIO_PIN_PC10;
 //    PIO_PIN NANDCLE  = PIO_PIN_PC17;
 //    PIO_PIN NANDALE = PIO_PIN_PC16;
 //    PIO_PIN NCS = PIO_PIN_PD18;
-
-    // WIP enum
-    enum ReadyBusyPin : PIO_PIN {
-        MemoryPartition1 = MEM_NAND_BUSY_1_PIN,
-        MemoryPartition2 = MEM_NAND_BUSY_2_PIN
-    };
 
 public:
     constexpr MT29F(ChipSelect chipSelect, PIO_PIN nandReadyBusyPin) : SMC(chipSelect), nandReadyBusyPin(nandReadyBusyPin) {}
