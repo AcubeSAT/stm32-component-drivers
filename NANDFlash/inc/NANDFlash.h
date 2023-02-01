59b891404c8477c52fe771c21afcc3c2c6750ab2
Stelios Tzelepis <stel.tze09@gmail.com>
2023-02-01 22:21:43 +0200

    Rename include and source files

--- NANDFlash/inc/NANDFlash.h
+++ NANDFlash/inc/NANDFlash.h
@@ -1,7 +1,6 @@
 #pragma once
-
-#include <cstdint>
-#include "definitions.h"
+#include "SMC.hpp"
+//#include "definitions.h"
 #include "FreeRTOS.h"
 #include "task.h"
 
@@ -11,17 +10,17 @@
  * @see http://ww1.microchip.com/downloads/en/DeviceDoc/NAND-Flash-Interface-with-EBI-on-Cortex-M-Based-MCUs-DS90003184A.pdf
  *  and https://gr.mouser.com/datasheet/2/671/micron_technology_micts05995-1-1759202.pdf
  */
-class MT29F {
+class MT29F  {
 private:
     uint32_t dataRegister = 0x61000000;
     uint32_t addressRegister = 0x61200000;
     uint32_t commandRegister = 0x61400000;
 
-    PIO_PIN NANDOE = PIO_PIN_PC9;
-    PIO_PIN NANDWE = PIO_PIN_PC10;
-    PIO_PIN NANDCLE  = PIO_PIN_PC17;
-    PIO_PIN NANDALE = PIO_PIN_PC16;
-    PIO_PIN NCS = PIO_PIN_PD18;
+//    PIO_PIN NANDOE = PIO_PIN_PC9;
+//    PIO_PIN NANDWE = PIO_PIN_PC10;
+//    PIO_PIN NANDCLE  = PIO_PIN_PC17;
+//    PIO_PIN NANDALE = PIO_PIN_PC16;
+//    PIO_PIN NCS = PIO_PIN_PD18;
 
 public:
 
--- NANDFlash/src/NANDFlash.cpp
+++ NANDFlash/src/NANDFlash.cpp
@@ -1,26 +1,26 @@
 #include "NANDFlash.h"
 
-uint8_t MT29F::initialize(){
-
-    uint8_t data;
-    while(1) {
-        if (PIO_PinRead(PIO_PIN_PA12)) {
-            sendCommand(0xFF);
-            while (1) {
-                if (PIO_PinRead(PIO_PIN_PA12)) {
-                    sendCommand(0x70);
-                    break;
-                }
-            }
-            PIO_PinWrite(NCS, false);
-            for(int i = 0; i < 10; i++){
-                data = *((volatile uint8_t *) dataRegister);
-            }
-            break;
-        }
-    }
-    return data;
-}
+//uint8_t MT29F::initialize(){
+//
+//    uint8_t data;
+//    while(1) {
+//        if (PIO_PinRead(PIO_PIN_PA12)) {
+//            sendCommand(0xFF);
+//            while (1) {
+//                if (PIO_PinRead(PIO_PIN_PA12)) {
+//                    sendCommand(0x70);
+//                    break;
+//                }
+//            }
+//            PIO_PinWrite(NCS, false);
+//            for(int i = 0; i < 10; i++){
+//                data = *((volatile uint8_t *) dataRegister);
+//            }
+//            break;
+//        }
+//    }
+//    return data;
+//}
 
 
 void MT29F::READ_ID() {
