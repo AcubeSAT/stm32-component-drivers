74e65b943e9d8add84de2dfe761ba528cb3c60b0
Stelios Tzelepis <stel.tze09@gmail.com>
2023-03-14 00:31:19 +0200

    Add documentation

--- LCL/inc/LCL.hpp
+++ LCL/inc/LCL.hpp
@@ -13,36 +13,53 @@
  * The LCLs are completely programmable during flight.
  * For now the class contains functionality only for the On-Board Computer Subsystem but
  * the Science Unit will make use of LCL circuits as well.
- * The main components that constitute a Latchup Current Limiter are a TLC555 timer, an
- * Operational Amplifier, a P-MOSFET, a N-MOSFET
+ * The main components that constitute a Latch up Current Limiter are a TLC555 timer, an
+ * Operational Amplifier, a P-MOSFET, a N-MOSFET.
+ * The programmable logic requires a Pulse Width Modulation (PWM) signal, a GPIO for Set and a GPIO for Reset Logic.
  */
 class LCL {
 private:
-
-    const PWM_CHANNEL_MASK pwmChannelMask;
+    /**
+     * The Pulse Width Modulation (PWM) channel used for setting the CONT voltage of the LCL.
+     */
     const PWM_CHANNEL_NUM pwmChannel;
-    const PIO_PIN resetPin = PIO_PIN_NONE;
-    const PIO_PIN setPin = PIO_PIN_NONE;
-    uint16_t pwmDutyCycle = 0;
-    uint16_t pwmFrequency = 0;
-    bool lclEnabled = false;
-    float currentThreshold = 0;
-public:
 
-    LCL(PWM_CHANNEL_MASK pwmChannelMask, PWM_CHANNEL_NUM pwmChannel, PIO_PIN resetPin, PIO_PIN setPin)
-        : pwmChannelMask(pwmChannelMask), pwmChannel(pwmChannel), resetPin(resetPin), setPin(setPin) {
+    /**
+     * The Pulse Width Modulation (PWM) channel mask used as a parameter for PWM peripheral functions.
+     */
+    const PWM_CHANNEL_MASK pwmChannelMask;
+
+    /**
+     * The Reset Pin force disables the LCL when driven Low, overriding the Set Pin.
+     * Drive High to start the sequence that enables the LCL.
+     */
+    const PIO_PIN resetPin = PIO_PIN_NONE;
+
+    /**
+     * The Set Pin force enables the LCL when driven Low. If the Set and Reset pins are High and the CONT voltage is
+     * higher than the THRES voltage, the LCL maintains its status.
+     */
+    const PIO_PIN setPin = PIO_PIN_NONE;
+
+public:
+    /**
+     * Constructor to set the necessary control pins for the LCL.
+     * @param pwmChannel
+     * @param pwmChannelMask
+     * @param resetPin
+     * @param setPin
+     */
+    LCL(PWM_CHANNEL_NUM pwmChannel, PWM_CHANNEL_MASK pwmChannelMask, PIO_PIN resetPin, PIO_PIN setPin)
+            : pwmChannel(pwmChannel), pwmChannelMask(pwmChannelMask), resetPin(resetPin), setPin(setPin) {
         PIO_PinWrite(resetPin, false);
         PIO_PinWrite(setPin, true);
-    }
-
-    inline void changePWMDutyCycle(uint16_t newDutyCycle) {
-        pwmDutyCycle = newDutyCycle;
-        PWM0_ChannelDutySet(pwmChannel, newDutyCycle);
     }
 
     void enableLCL();
 
     void disableLCL();
+
+    void changePWMDutyCycle(uint16_t newDutyCycle);
 
     void returnLCLStatus();
 
--- LCL/src/LCL.cpp
+++ LCL/src/LCL.cpp
@@ -4,11 +4,11 @@ void LCL::enableLCL() {
     // Todo assert status of GPIOs/log
     PIO_PinWrite(resetPin, true); // RESET
     PWM0_ChannelsStart(pwmChannelMask);
-
     PIO_PinWrite(setPin, false);
+
     vTaskDelay(pdMS_TO_TICKS(10));
+
     PIO_PinWrite(setPin, true);
-    lclEnabled = true;
 }
 
 
@@ -16,5 +16,4 @@ void LCL::disableLCL() {
     // Todo assert status of GPIOs/log
     PWM0_ChannelsStop(pwmChannelMask);
     PIO_PinWrite(resetPin, false);
-    lclEnabled = false;
 }
