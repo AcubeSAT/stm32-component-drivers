aaa836827760a4d333ccbc5065bb1d041ca483ed
Dimitris_Koukourikos <dimitrioskoukourikos@gmail.com>
2023-06-25 14:26:46 +0300

    Created "getTemperature" function.

--- AD590-Driver/inc/AD590_Driver.hpp
+++ AD590-Driver/inc/AD590_Driver.hpp
@@ -1,11 +1,14 @@
-
 #ifndef OBC_SOFTWARE_AD590_DRIVER_HPP
 #define OBC_SOFTWARE_AD590_DRIVER_HPP
 
 #include "src/config/default/peripheral/afec/plib_afec0.h"
 
 class AD590 {
-public:
+private:
+    /**
+     * Gets the analog temperature from the AD590 temperature sensor and converts it to digital.
+     * @return The temperature as a float
+     */
     float getTemperature();
 };
 #endif //OBC_SOFTWARE_AD590_DRIVER_HPP
--- AD590-Driver/src/AD590_Driver.cpp
+++ AD590-Driver/src/AD590_Driver.cpp
@@ -1,6 +1,7 @@
 #include "AD590_Driver.hpp"
 
 float AD590::getTemperature() {
+    uint16_t data = 0;
     float result = 0;
 
     AFEC0_ChannelsEnable(AFEC_CH0_MASK);
@@ -8,11 +9,29 @@ float AD590::getTemperature() {
 
     AFEC0_ConversionStart();
 
+    while(!AFEC0_ChannelResultIsReady(AFEC_CH0) || !AFEC0_ChannelResultIsReady(AFEC_CH1))
+    {
+    };
+
     if(AFEC0_ChannelResultIsReady(AFEC_CH0)){
-        result = result = AFEC0_ChannelResultGet(AFEC_CH0);
+        if (AFEC0_ChannelResultGet(AFEC_CH0) != NULL){
+            data =  AFEC0_ChannelResultGet(AFEC_CH0);
+        }
     }
     else if(AFEC0_ChannelResultIsReady(AFEC_CH1)){
-        result = result = AFEC0_ChannelResultGet(AFEC_CH1);
+        if (AFEC0_ChannelResultGet(AFEC_CH1) != NULL){
+            data =  AFEC0_ChannelResultGet(AFEC_CH1);
+        }
+    }
+
+    uint8_t upperByte = (data >> 8) & 0x1F;
+    uint8_t lowerByte = data & 0xFF;
+
+    if ((upperByte & 0x10) != 0) {
+        upperByte &= 0x0F;
+        result = -(256 - (upperByte * 16.0f + lowerByte / 16.0f));
+    } else {
+        result = upperByte * 16.0f + lowerByte / 16.0f;
     }
 
     return result;
