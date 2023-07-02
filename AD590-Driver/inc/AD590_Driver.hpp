090539d574c92c21690e3a4dd8939de82a967306
Dimitris_Koukourikos <dimitrioskoukourikos@gmail.com>
2023-07-02 17:14:37 +0300

    Class documentation and adjustments.

--- AD590-Driver/inc/AD590_Driver.hpp
+++ AD590-Driver/inc/AD590_Driver.hpp
@@ -1,14 +1,24 @@
-#ifndef OBC_SOFTWARE_AD590_DRIVER_HPP
-#define OBC_SOFTWARE_AD590_DRIVER_HPP
+#pragma once
 
-#include "src/config/default/peripheral/afec/plib_afec0.h"
+#include "peripheral/afec/plib_afec0.h"
+
+/**
+ * AD590 temperature sensor driver
+ *
+ * This is a simple driver to use the AD590 sensor on ATSAMV71Q21B microcontrollers.
+ *
+ * For more details about the operation of the sensor, see the datasheets found at:
+ * https://www.mouser.com/catalog/specsheets/intersil_fn3171.pdf
+ * and https://www.analog.com/media/en/technical-documentation/data-sheets/ad590.pdf
+ */
 
 class AD590 {
-private:
+public:
+
     /**
      * Gets the analog temperature from the AD590 temperature sensor and converts it to digital.
      * @return The temperature as a float
      */
     float getTemperature();
 };
-#endif //OBC_SOFTWARE_AD590_DRIVER_HPP
+
