faaa21a6311fc0093df86b94f8d43bc053ae1524
Stelios Tzelepis <stel.tze09@gmail.com>
2023-07-28 14:22:29 +0300

    Fix volt to temp fun

--- AD590/src/AD590_Driver.cpp
+++ AD590/src/AD590_Driver.cpp
@@ -5,7 +5,7 @@ void AD590::getTemperature() {
     AFEC0_ConversionStart();
     vTaskDelay(pdMS_TO_TICKS(1));
 
-        if(!AFEC0_ChannelResultIsReady(AFEC_CH0)){}
+        while(!AFEC0_ChannelResultIsReady(AFEC_CH0)){}
 
             uint16_t ADCconversion = AFEC0_ChannelResultGet(AFEC_CH0);
 //            float voltageConversion = static_cast<float>(ADCconversion) * PositiveVoltageReference / MaxADCValue;
@@ -26,7 +26,7 @@ void AD590::getTemperature() {
 }
 
 float AD590::convertADCValueToTemperature(uint16_t ADCconversion){
-    float voltageConversion = static_cast<float>(ADCconversion)/4096*3.3;
-    float currentConversion = voltageConversion/7870;
+    float voltageConversion = static_cast<float>(ADCconversion)/4096*3300;
+    float currentConversion = voltageConversion/7.870;
     return currentConversion - offsetCurrent + referenceTemperature ;
 }
