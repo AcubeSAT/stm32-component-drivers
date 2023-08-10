843f2f9a57a0818fc42db83cf882b1cdf0d656b3
kchristin22 <christinakoutsou22@gmail.com>
2023-08-10 19:04:51 +0300

    Make write function more modular

--- NANDFlash/inc/NANDFlash.h
+++ NANDFlash/inc/NANDFlash.h
@@ -22,38 +22,66 @@ private:
 
     inline static constexpr uint8_t enableNandConfiguration = 1;
 
-    enum Commands : uint8_t {
-        RESET = 0xFF,
-        READID = 0x90,
-        READ_PARAM_PAGE = 0xEC,
-        READ_UNIQ_ID = 0xED,
-        SET_FEATURE = 0xEF,
-        GET_FEATURE = 0xEE,
-        READ_STATUS = 0x70,
-        READ_STATUS_ENHANCED = 0x78,
-        ERASE_BLOCK = 0x60,
-        ERASE_BLOCK_CONFIRM = 0xD0,
-        READ_MODE = 0x00,
-        READ_CONFIRM = 0x30,
-        PAGE_PROGRAM = 0x80,
-        PAGE_PROGRAM_CONFIRM = 0x10,
-        READ_INTERNAL_DATA_MOVE = 0x35,
-        PROGRAM_INTERNAL_DATA_MOVE = 0x85,
-        LOCK = 0x2A,
-        BLOCK_UNLOCK_LOW = 0x23,
-        BLOCK_UNLOCK_HIGH = 0x24,
-        BLOCK_LOCK_READ_STATUS = 0x7A
+    enum Commands : const uint8_t{
+            RESET = 0xFF,
+            READID = 0x90,
+            READ_PARAM_PAGE = 0xEC,
+            READ_UNIQ_ID = 0xED,
+            SET_FEATURE = 0xEF,
+            GET_FEATURE = 0xEE,
+            READ_STATUS = 0x70,
+            READ_STATUS_ENHANCED = 0x78,
+            ERASE_BLOCK = 0x60,
+            ERASE_BLOCK_CONFIRM = 0xD0,
+            READ_MODE = 0x00,
+            READ_CONFIRM = 0x30,
+            PAGE_PROGRAM = 0x80,
+            PAGE_PROGRAM_CONFIRM = 0x10,
+            READ_INTERNAL_DATA_MOVE = 0x35,
+            PROGRAM_INTERNAL_DATA_MOVE = 0x85,
+            LOCK = 0x2A,
+            BLOCK_UNLOCK_LOW = 0x23,
+            BLOCK_UNLOCK_HIGH = 0x24,
+            BLOCK_LOCK_READ_STATUS = 0x7A
     };
 
-    /* The number of bytes a page contains*/
+    /* Number of bytes contained in a page */
     const static inline uint16_t PageSizeBytes = 8640;
-    /* The number of bytes a block contains*/
+    /* Number of bytes contained in a block */
     const static inline uint32_t BlockSizeBytes = 1105920;
+    /* Number of pages contained in a block */
+    const static inline uint8_t NumPagesBlock = 128;
+    /* Total number of pages in an LUN */
+    const static inline uint16_t MaxNumPage = 524288;
+    /* Total number of blocks in a LUN */
+    const static inline uint16_t MaxNumBlock = 4096;
     /* The ARDY bit is bit 5 of the Status Register */
     const static inline uint8_t ArrayReadyMask = 0x20;
+    /* Chunk size of data to be written in addition to ECC parity bits */
+    const static inline uint16_t WriteChunkSize = 512;
+    /* Max number of chunks contained in a page */
+    const static inline uint8_t MaxChunksPerPage = PageSizeBytes / WriteChunkSize;
+    /* Number of ECC bytes per write data chunk */
+    const static inline uint16_t NumECCBytes = 3;
+    /* Max number of data bytes available in a page including ECC bytes */
+    const static inline uint16_t MaxDataBytesPerPage = PageSizeBytes - (MaxChunksPerPage * NumECCBytes);
+
 
     struct Address {
         uint8_t col1, col2, row1, row2, row3;
+    };
+
+    enum AddressConfig : const uint8_t{
+            POS = 0x01,
+            PAGE,
+            POS_PAGE,
+            PAGE_BLOCK
+    };
+
+    struct Structure {
+        uint64_t position = MaxNumPage * PageSizeBytes; // overall position inside the LUN or inside a specific page
+        uint16_t page = MaxNumPage, block = MaxNumBlock; // overall page number or number of page inside a single block
+        uint8_t LUN = 2;
     };
 
     const static inline uint8_t TimeoutCycles = 20;
@@ -118,17 +146,58 @@ public:
 
     void readNANDID(etl::array<uint8_t, 8> id);
 
-    Address setAddress(uint8_t LUN, uint32_t position);
+    Address setAddress(Structure *pos, AddressConfig mode);
 
-    bool writeNAND(uint8_t LUN, uint32_t position, uint8_t data);
+    constexpr static bool isValidStructure(const Structure *pos,const AddressConfig op);
+
+    bool writeNAND(Structure *pos, AddressConfig op, uint8_t data);
+
+    template<unsigned int size, uint32_t numberOfAddresses>
+    bool writeNAND(Structure *pos, AddressConfig op, etl::array<uint8_t, size> data) {
+        static_assert((numberOfAddresses > size), "Number of addresses is bigger than data size");
+        if(!isValidStructure(pos, op)) return false;
+        if (op == POS || op == POS_PAGE) {
+            uint8_t page = pos->position / PageSizeBytes;
+            const uint16_t column = pos->position - page * PageSizeBytes;
+            if((column + size - numberOfAddresses + NumECCBytes) > PageSizeBytes) return false;
+        }
+
+        const Address writeAddress = setAddress(pos, op);
+        sendCommand(PAGE_PROGRAM);
+        sendAddress(writeAddress.col1);
+        sendAddress(writeAddress.col2);
+        sendAddress(writeAddress.row1);
+        sendAddress(writeAddress.row2);
+        sendAddress(writeAddress.row3);
+        for (uint16_t i = 0; i < numberOfAddresses; i++) {
+            if (waitDelay()) {
+                return !NANDisReady;
+            }
+            sendData(data[i]);
+        }
+        sendCommand(PAGE_PROGRAM_CONFIRM);
+        return !detectArrayError();
+    }
+
 
     template<unsigned int size>
-    bool writeNAND(uint8_t LUN, uint32_t position, uint32_t numberOfAddresses, etl::array<uint8_t, size> data) {
+    bool writeNANDPos(Structure *pos, uint32_t numberOfAddresses, etl::array<uint8_t, size> data) {
         static_assert(size < PageSizeBytes, "Data to be written in NAND surpass the Page limit");
-        if (numberOfAddresses > data.size()){
+        if (numberOfAddresses > data.size()) {
             return !NANDisReady;
         }
-        const Address writeAddress = setAddress(LUN, position);
+        uint8_t page = pos->position / PageSizeBytes;
+        const uint16_t column = pos->position - page * PageSizeBytes;
+        if ((column + numberOfAddresses + NumECCBytes) > PageSizeBytes) {
+            if (++page != NumPagesBlock) {
+                pos->page++;
+                // TODO: add read to see if that page is written already up to numberOfAddresses + NumECCBytes
+                writeNANDPage(pos, numberOfAddresses, data);
+            } else return !NANDisReady;
+        }
+
+        if (pos->LUN > 1) return !NANDisReady;
+        const Address writeAddress = setAddress(pos, POS);
         sendCommand(PAGE_PROGRAM);
         sendAddress(writeAddress.col1);
         sendAddress(writeAddress.col2);
@@ -147,28 +216,28 @@ public:
 
     bool readNAND(uint8_t data, uint8_t LUN, uint32_t position);
 
-    template <unsigned int size>
-    bool readNAND(etl::array<uint8_t, size> data, uint8_t LUN, uint32_t start_position, uint32_t numberOfAddresses) {
-        static_assert(size < PageSizeBytes, "Data to be read from NAND surpass the Page limit");
-        if (numberOfAddresses > size){
-            return !NANDisReady;
-        }
-        const Address readAddress = setAddress(LUN, start_position);
-        sendCommand(READ_MODE);
-        sendAddress(readAddress.col1);
-        sendAddress(readAddress.col2);
-        sendAddress(readAddress.row1);
-        sendAddress(readAddress.row2);
-        sendAddress(readAddress.row3);
-        sendCommand(READ_CONFIRM);
-        for (uint16_t i = 0; i < numberOfAddresses; i++) {
-            if (waitDelay()) {
-                return !NANDisReady;
-            }
-            data[i] = readData();
-        }
-        return NANDisReady;
-    }
+//    template <unsigned int size>
+//    bool readNAND(etl::array<uint8_t, size> data, uint8_t LUN, uint32_t start_position, uint32_t numberOfAddresses) {
+//        static_assert(size < PageSizeBytes, "Data to be read from NAND surpass the Page limit");
+//        if (numberOfAddresses > size){
+//            return !NANDisReady;
+//        }
+//        const Address readAddress = setAddress(LUN, start_position);
+//        sendCommand(READ_MODE);
+//        sendAddress(readAddress.col1);
+//        sendAddress(readAddress.col2);
+//        sendAddress(readAddress.row1);
+//        sendAddress(readAddress.row2);
+//        sendAddress(readAddress.row3);
+//        sendCommand(READ_CONFIRM);
+//        for (uint16_t i = 0; i < numberOfAddresses; i++) {
+//            if (waitDelay()) {
+//                return !NANDisReady;
+//            }
+//            data[i] = readData();
+//        }
+//        return NANDisReady;
+//    }
 
     bool eraseBlock(uint8_t LUN, uint16_t block);
 
--- NANDFlash/src/NANDFlash.cpp
+++ NANDFlash/src/NANDFlash.cpp
@@ -12,24 +12,52 @@ uint8_t MT29F::resetNAND() {
 }
 
 
-MT29F::Address MT29F::setAddress(uint8_t LUN, uint32_t position) {
-    const uint8_t page = position / PageSizeBytes;
-    const uint16_t column = position - page * PageSizeBytes;
-    const uint16_t block = position / BlockSizeBytes;
+MT29F::Address MT29F::setAddress(MT29F::Structure *pos, MT29F::AddressConfig mode) {
+    uint16_t column = pos->position;
+    switch (mode) {
+        case POS:
+            column -= pos->page * PageSizeBytes;
+            pos->page = pos->position / PageSizeBytes;
+        case POS_PAGE:
+            pos->block = pos->position / BlockSizeBytes;
+            break;
+        case PAGE:
+            column = 0;
+            pos->block = pos->page / NumPagesBlock;
+            pos->page = pos->page - pos->block * NumPagesBlock;
+    }
+
 
     Address address;
     address.col1 = column & 0xff;
     address.col2 = (column & 0x3f00) >> 8;
-    address.row1 = (page & 0x7f) | ((block & 0x01) << 7);
-    address.row2 = (block >> 1) & 0xff;
-    address.row3 = ((block >> 9) & 0x07) | ((LUN & 0x01) << 3);
+    address.row1 = (pos->page & 0x7f) | ((pos->block & 0x01) << 7);
+    address.row2 = (pos->block >> 1) & 0xff;
+    address.row3 = ((pos->block >> 9) & 0x07) | ((pos->LUN & 0x01) << 3);
 
     return address;
 }
 
+constexpr bool MT29F::isValidStructure(const MT29F::Structure *pos,const MT29F::AddressConfig op) {
+    if (pos->LUN > 1) return false;
+    switch (op) {
+        case POS:
+            if (pos->position >= ((MaxNumPage * PageSizeBytes) - (MaxChunksPerPage * NumECCBytes))) return false;
+            break;
+        case POS_PAGE:
+            if (pos->position >= MaxDataBytesPerPage) return false;
+            break;
+        case PAGE:
+            if (pos->page >= MaxNumPage) return false;
+            break;
+        case PAGE_BLOCK:
+            if (pos->page >= NumPagesBlock || pos->block >= MaxNumBlock) return false;
+    }
+    return true;
+}
 
 void MT29F::readNANDID(etl::array<uint8_t, 8> id) {
-    static_assert(id.size()==8, "NAND's ID array is not 8 digits");
+    static_assert(id.size() == 8, "NAND's ID array is not 8 digits");
     sendCommand(READID);
     sendAddress(READ_MODE);
     for (uint8_t i = 0; i < 8; i++) {
@@ -37,9 +65,10 @@ void MT29F::readNANDID(etl::array<uint8_t, 8> id) {
     }
 }
 
+bool MT29F::writeNAND(MT29F::Structure *pos, MT29F::AddressConfig op, uint8_t data) {
+    if(!isValidStructure(pos, op)) return false;
 
-bool MT29F::writeNAND(uint8_t LUN, uint32_t position, uint8_t data) {
-    const Address writeAddress = setAddress(LUN, position);
+    const Address writeAddress = setAddress(pos, PAGE);
     sendCommand(PAGE_PROGRAM);
     sendAddress(writeAddress.col1);
     sendAddress(writeAddress.col2);
@@ -51,21 +80,22 @@ bool MT29F::writeNAND(uint8_t LUN, uint32_t position, uint8_t data) {
     return !detectArrayError();
 }
 
-bool MT29F::readNAND(uint8_t data, uint8_t LUN, uint32_t position) {
-    const Address readAddress = setAddress(LUN, position);
-    sendCommand(READ_MODE);
-    sendAddress(readAddress.col1);
-    sendAddress(readAddress.col2);
-    sendAddress(readAddress.row1);
-    sendAddress(readAddress.row2);
-    sendAddress(readAddress.row3);
-    sendCommand(READ_CONFIRM);
-    if (waitDelay() == 0) {
-        return !NANDisReady;
-    }
-    data = readData();
-    return NANDisReady;
-}
+
+//bool MT29F::readNAND(uint8_t data, uint8_t LUN, uint32_t position) {
+//    const Address readAddress = setAddress(LUN, position);
+//    sendCommand(READ_MODE);
+//    sendAddress(readAddress.col1);
+//    sendAddress(readAddress.col2);
+//    sendAddress(readAddress.row1);
+//    sendAddress(readAddress.row2);
+//    sendAddress(readAddress.row3);
+//    sendCommand(READ_CONFIRM);
+//    if (waitDelay() == 0) {
+//        return !NANDisReady;
+//    }
+//    data = readData();
+//    return NANDisReady;
+//}
 
 
 bool MT29F::eraseBlock(uint8_t LUN, uint16_t block) {
@@ -94,10 +124,10 @@ bool MT29F::detectArrayError() {
 }
 
 bool MT29F::isNANDAlive() {
-    etl::array<uint8_t,8> id = {};
+    etl::array<uint8_t, 8> id = {};
     const uint8_t valid_id[8] = {0x2C, 0x68, 0x00, 0x27, 0xA9, 0x00, 0x00, 0x00};
     readNANDID(id);
-    if (etl::equal(id.begin(),id.end(), valid_id)) {
+    if (etl::equal(id.begin(), id.end(), valid_id)) {
         return true;
     } else return false;
 }
@@ -121,3 +151,4 @@ bool MT29F::errorHandler() {
     }
     return NANDisReady;
 }
+
