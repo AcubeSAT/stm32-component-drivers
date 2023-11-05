54701cb0cf64fe8a43499ae83f32ca63ea733b5c
steltze <stel.tze09@gmail.com>
2023-11-05 22:41:49 +0000

    Merge the ATSAM specific part of the NAND driver

---
+++ NANDFlash/MT29F_Nand_Driver_Micron/common.hpp
@@ -0,0 +1,35 @@
+/*******************************************************************************
+
+  Filename:		common.h
+  Description:  Common data type for Micron NAND Drivers
+
+  � 2012 Micron Technology, Inc. All Rights Reserved
+
+ You have a license to reproduce, display, perform, produce derivative works of,
+ and distribute (in original or modified form) the Program, provided that you
+ explicitly agree to the following disclaimer:
+
+   THIS PROGRAM IS PROVIDED "AS IT IS" WITHOUT WARRANTY OF ANY KIND, EITHER
+   EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO, THE IMPLIED WARRANTY
+   OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE ENTIRE RISK
+   AS TO THE QUALITY AND PERFORMANCE OF THE PROGRAM IS WITH YOU. SHOULD THE
+   PROGRAM PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL NECESSARY SERVICING,
+   REPAIR OR CORRECTION.
+
+********************************************************************************
+
+   Version History.
+
+   Ver.				No		Date     	Comments
+
+   First release	1.0		07/2012
+
+*******************************************************************************/
+
+/* basic data types */
+typedef unsigned char        MT_uint8;
+typedef signed char          MT_sint8;
+typedef unsigned short       MT_uint16;
+typedef signed short         MT_sint16;
+typedef unsigned int         MT_uint32;
+typedef signed int           MT_sint32;
