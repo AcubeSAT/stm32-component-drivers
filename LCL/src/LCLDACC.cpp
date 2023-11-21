01da0104bd8c928b6370c658ee56fb33b100b8c3
ktsoupos <konstantinostsoupos@gmail.com>
2023-11-21 10:20:15 +0200

    check DACC availibility with loop

--- LCL/src/LCLDACC.cpp
+++ LCL/src/LCLDACC.cpp
@@ -6,33 +6,25 @@ LCLDACC::LCLDACC(DACC_CHANNEL_NUM dacChannel, PIO_PIN resetPin, PIO_PIN setPin):
 }
 
 void LCLDACC::enableLCL() {
-    DACC_DataWrite (dacChannel, 0xff);//value
-    if (DACC_IsReady(dacChannel))
-    {
-        DACC_DataWrite (dacChannel, 0xff);//value
+    DACC_DataWrite(dacChannel, 0xff); // Set initial data value
+
+    while (!DACC_IsReady(dacChannel)) {
+        // Wait until DACC is ready
     }
-    else{
-        //error handling code here
-        DACC_Initialize();
-    }
+    DACC_DataWrite(dacChannel, 0xff); // Set data value again after ensuring DACC is ready
     PIO_PinWrite(setPin, false);
-
     vTaskDelay(pdMS_TO_TICKS(10));
-
     PIO_PinWrite(setPin, true);
 }
 
 void LCLDACC::disableLCL() {
     // Disable the DACC channel (dacChannel)
-    DACC_DataWrite (dacChannel, 0);
-    if (DACC_IsReady(dacChannel))
-    {
-        DACC_DataWrite (dacChannel, 0);
-    }
-    else{
-        DACC_Initialize();
-    }
+    DACC_DataWrite (dacChannel, 0x00);
 
+    while (!DACC_IsReady(dacChannel)) {
+        // Wait until DACC is ready
+    }
+    DACC_DataWrite (dacChannel, 0x00); // Set data value again after ensuring DACC is ready
     // Drive resetPin and setPin as needed for DAC operation
     PIO_PinWrite(resetPin, false);
     PIO_PinWrite(setPin, true);
