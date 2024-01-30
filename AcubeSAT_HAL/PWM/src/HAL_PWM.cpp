8250b5234c44946e9f59507d154e73a91e24dcd9
Konstantinos Tsoupos <konstantinostsoupos@gmail.com>
2024-01-30 21:10:44 +0000

    Dacc lcl

---
+++ AcubeSAT_HAL/PWM/src/HAL_PWM.cpp
@@ -0,0 +1,46 @@
+#include "HAL_PWM.hpp"
+
+template<uint8_t peripheralNumber>
+void HAL_PWM::PWM_ChannelsStart(PWM_CHANNEL_MASK channelMask) {
+    static_assert(peripheralNumber == 0 || peripheralNumber == 1, "Template parameter must be 0 or 1");
+}
+
+template<>
+void HAL_PWM::PWM_ChannelsStart<0>(PWM_CHANNEL_MASK channelMask) {
+    PWM0_ChannelsStart(channelMask);
+}
+
+template<>
+void HAL_PWM::PWM_ChannelsStart<1>(PWM_CHANNEL_MASK channelMask) {
+    PWM1_ChannelsStart(channelMask);
+}
+
+template<uint8_t peripheralNumber>
+void HAL_PWM::PWM_ChannelsStop(PWM_CHANNEL_MASK channelMask) {
+    static_assert(peripheralNumber == 0 || peripheralNumber == 1, "Template parameter must be 0 or 1");
+}
+
+template<>
+void HAL_PWM::PWM_ChannelsStop<0>(PWM_CHANNEL_MASK channelMask) {
+    PWM0_ChannelsStop(channelMask);
+}
+
+template<>
+void HAL_PWM::PWM_ChannelsStop<1>(PWM_CHANNEL_MASK channelMask) {
+    PWM1_ChannelsStop(channelMask);
+}
+
+template<uint8_t peripheralNumber>
+void HAL_PWM::PWM_ChannelDutySet(PWM_CHANNEL_NUM pwmChannel, uint16_t dutyCycle) {
+    static_assert(peripheralNumber == 0 || peripheralNumber == 1, "Template parameter must be 0 or 1");
+}
+
+template<>
+void HAL_PWM::PWM_ChannelDutySet<0>(PWM_CHANNEL_NUM pwmChannel, uint16_t dutyCycle) {
+    PWM0_ChannelDutySet(pwmChannel, dutyCycle);
+}
+
+template<>
+void HAL_PWM::PWM_ChannelDutySet<1>(PWM_CHANNEL_NUM pwmChannel, uint16_t dutyCycle) {
+    PWM1_ChannelDutySet(pwmChannel, dutyCycle);
+}
--- LCL/inc/LCL.hpp
+++ LCL/inc/LCL.hpp
@@ -2,7 +2,6 @@
 
 #include <cstdint>
 #include "peripheral/pio/plib_pio.h"
-#include "peripheral/pwm/plib_pwm0.h"
 #include "FreeRTOS.h"
 #include "task.h"
 
@@ -11,20 +10,10 @@
  * The LCLs are completely reprogrammable during flight.
  * The main components that constitute a Latch up Current Limiter are a TLC555 timer, an
  * Operational Amplifier, a P-MOSFET, a N-MOSFET.
- * The programmable logic requires a Pulse Width Modulation (PWM) signal, a GPIO for Set and a GPIO for Reset Logic.
+ * The programmable logic requires a Pulse Width Modulation (PWM) signal or Digital to Analog Converter (DAC), a GPIO for Set and a GPIO for Reset Logic.
  */
 class LCL {
-private:
-    /**
-     * The Pulse Width Modulation (PWM) channel used for setting the CONT voltage of the LCL.
-     */
-    const PWM_CHANNEL_NUM pwmChannel;
-
-    /**
-     * The Pulse Width Modulation (PWM) channel mask used as a parameter for PWM peripheral functions.
-     */
-    const PWM_CHANNEL_MASK pwmChannelMask;
-
+protected:
     /**
      * The Reset Pin force disables the LCL when driven Low, overriding the Set Pin.
      * Drive High to start the sequence that enables the LCL.
@@ -37,36 +26,20 @@ private:
      */
     const PIO_PIN setPin = PIO_PIN_NONE;
 
-public:
     /**
      * Constructor to set the necessary control pins for the LCL.
-     * @param pwmChannel @see pwmChannel
-     * @param pwmChannelMask @see pwmChannelMask
      * @param resetPin @see resetPin
      * @param setPin @see setPin
      */
-    LCL(PWM_CHANNEL_NUM pwmChannel, PWM_CHANNEL_MASK pwmChannelMask, PIO_PIN resetPin, PIO_PIN setPin);
+    LCL(PIO_PIN resetPin, PIO_PIN setPin) : resetPin(resetPin), setPin(setPin) {}
 
     /**
-     * Enable to LCL to monitor and protect the protected IC from over current.
-     * Sequence to enable the LCL is as follows:
-     * - Enable the PWM signal with predefined frequency and duty cycle to form the desired current threshold.
-     * - Pull the Reset Pin High to allow SR Latch state of the TLC555 to be driven but the internal comparators
-     * instead of being forced Low.
-     * - Pull the Set Pin Low to force the SR Latch state of the TLC555 to logic High.
-     * - After a brief delay that is necessary for the TLC555 to detect the pulse, pull the Set Pin High making the SR
-     * Latch retain its state. If the it was High, the IC is powered on, if a current surge is detected, the SR Latch
-     * will be driven Low, cutting the power supply towards the protected IC and retain its state after the surge.
-     * @note If a surge is detected and the power is cut, the MCU will have to call the @fn enableLCL to provide the IC
-     * with power again.
+     * Enable the LCL
      */
-    void enableLCL();
+    virtual void enableLCL() = 0;
 
     /**
-     * Disable the LCL, cutting the supply voltage to the IC. To achieve this, the Reset Pin is driven Low to force the
-     * SR Latch state to Low, cutting the power towards the IC and as an extra step, the PWM signal is closed, setting
-     * the current threshold to a small value, typically much smaller than the consumption of the protected IC.
-     * @note Configure the default state of the PWM signal to be Low (instead of High) when the channel is turned off.
+     * Disable the LCL
      */
-    void disableLCL();
+    virtual void disableLCL() = 0;
 };
