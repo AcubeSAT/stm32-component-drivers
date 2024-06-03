3c41afe2ab45ca2fe2506583e3749fb352649ad4
Epatsatzaki <evapatsatzakis@gmail.com>
2024-06-03 20:05:46 +0300

    made HAL_PWM a namespace

--- AcubeSAT_HAL/PWM/inc/HAL_PWM.hpp
+++ AcubeSAT_HAL/PWM/inc/HAL_PWM.hpp
@@ -5,13 +5,12 @@
 #include "peripheral/pwm/plib_pwm1.h"
 
 /**
- * @brief HAL_PWM class for controlling PWM peripherals.
+ * @brief HAL_PWM namespace for controlling PWM peripherals.
  *
- * This class provides a high-level interface for controlling PWM peripherals.
+ * This namespace provides a high-level interface for controlling PWM peripherals.
  * It includes functions for starting and stopping channels and setting duty cycles.
  */
-class HAL_PWM {
-public:
+namespace HAL_PWM {
     /**
      * @brief Start PWM channels for a specific peripheral.
      *
@@ -21,7 +20,7 @@ public:
      * This function starts PWM channels for the specified peripheral.
      */
     template<uint8_t peripheralNumber>
-    static void PWM_ChannelsStart(PWM_CHANNEL_MASK channelMask);
+    void PWM_ChannelsStart(PWM_CHANNEL_MASK channelMask);
 
     /**
      * @brief Stop PWM channels for a specific peripheral.
@@ -32,7 +31,7 @@ public:
      * This function stops PWM channels for the specified peripheral.
      */
     template<uint8_t peripheralNumber>
-    static void PWM_ChannelsStop(PWM_CHANNEL_MASK channelMask);
+    void PWM_ChannelsStop(PWM_CHANNEL_MASK channelMask);
 
     /**
      * @brief Set duty cycle for a specific channel of a peripheral.
@@ -44,23 +43,24 @@ public:
      * This function sets the duty cycle for a specific channel of the specified peripheral.
      */
     template<uint8_t peripheralNumber>
-    static void PWM_ChannelDutySet(PWM_CHANNEL_NUM pwmChannel, uint16_t dutyCycle);
+    void PWM_ChannelDutySet(PWM_CHANNEL_NUM pwmChannel, uint16_t dutyCycle);
 
     /**
-     * @brief Gets period foρ a specific channel of a peripheral.
+     * @brief Gets period for a specific channel of a peripheral.
      *
      * @tparam peripheralNumber The peripheral number (0 or 1).
      * @param pwmChannel The PWM channel number.
      */
     template<uint8_t peripheralNumber>
-    static uint16_t PWM_ChannelPeriodGet(PWM_CHANNEL_NUM pwmChannel);
+    uint16_t PWM_ChannelPeriodGet(PWM_CHANNEL_NUM pwmChannel);
 
     /**
-   * @brief Sets period for a specific channel of a peripheral.
-   *
-   * @tparam peripheralNumber The peripheral number (0 or 1).
-   * @param pwmChannel The PWM channel number.
-   */
+     * @brief Sets period for a specific channel of a peripheral.
+     *
+     * @tparam peripheralNumber The peripheral number (0 or 1).
+     * @param pwmChannel The PWM channel number.
+     * @param period The period value (0 to 65535).
+     */
     template<uint8_t peripheralNumber>
-    static void PWM_ChannelPeriodSet(PWM_CHANNEL_NUM pwmChannel, uint16_t period);
-};
+    void PWM_ChannelPeriodSet(PWM_CHANNEL_NUM pwmChannel, uint16_t period);
+}
