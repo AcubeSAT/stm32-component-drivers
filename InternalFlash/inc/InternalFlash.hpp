49cb4a8c5baed6335dd34e539fa1d297cff27159
NPateras <npateras8@gmail.com>
2024-11-13 20:22:38 +0200

    code readability changes

--- InternalFlash/inc/InternalFlash.hpp
+++ InternalFlash/inc/InternalFlash.hpp
@@ -3,34 +3,49 @@
 #include <cstdint>
 #include "peripheral/pio/plib_pio.h"
 #include "plib_efc.h"
+#include "FreeRTOS.h"
+#include "Logger.hpp"
+#include "task.h"
 
 /**
  * Class to interact with the Internal FLASH Memory of the MCU.
  *
  * @brief This class provides functions with a generic implementation based on the EFC peripheral library for easy integration into any code.
  */
-class InternalFlash {
+class FlashDriver {
 public:
+
+    /**
+     * Wait period before an EFC transaction is skipped.
+     */
+    static constexpr uint16_t TimeoutTicks = 1000;
+
+
+    using FlashAddress = uint32_t;
+    using FlashData = uint32_t;
+    using FlashEraseLength = uint32_t;
     /**
      * Constructor to create an instance of the class and initialize the EFC peripheral.
      */
-    InternalFlash();
+    FlashDriver();
 
     /**
-     * Write function that writes 128 bits.
+     * Write function that writes 128 bits. Only ‘0’ values can be programmed using Flash technology; ‘1’ is the erased value. In order to
+     * program words in a page, the page must first be erased.
      * @param data pointer to 128-bit data buffer.
      * @param address FLASH address to be modified.
      * @return member of the EFC_ERROR enum.
      */
-    EFC_ERROR QuadWordWrite(uint32_t* data, uint32_t address);
+    static EFC_ERROR QuadWordWrite(FlashData* data, FlashAddress address);
 
     /**
-     * Write function that writes an entire page of 512 bytes.
+     * Write function that writes an entire page of 512 bytes. Only ‘0’ values can be programmed using Flash technology; ‘1’ is the erased value. In order to
+     * program words in a page, the page must first be erased.
      * @param data pointer to data buffer of size equivalent to page size.
      * @param address  	FLASH address to be modified.
      * @return member of the EFC_ERROR enum.
      */
-    EFC_ERROR PageWrite(uint32_t* data, uint32_t address);
+    static EFC_ERROR PageWrite(FlashData* data, FlashAddress address);
 
     /**
      * Reads length number of bytes from a given address in FLASH memory into
@@ -40,12 +55,25 @@ public:
      * @param address FLASH address to be read from.
      * @return member of the EFC_ERROR enum.
      */
-    EFC_ERROR Read(uint32_t* data, uint32_t length, uint32_t address);
+    static EFC_ERROR Read(FlashData* data, FlashEraseLength length, FlashAddress address);
 
     /**
      * This function is used to erase a sector.
      * @param address FLASH address to be Erased.
      * @return member of the EFC_ERROR enum.
      */
-    EFC_ERROR SectorErase(uint32_t address);
+    static EFC_ERROR SectorErase(FlashAddress address);
+
+private:
+    static void waitForResponse() {
+        auto start = xTaskGetTickCount();
+        while (EFC_IsBusy()) {
+            if (xTaskGetTickCount() - start > TimeoutTicks) {
+                LOG_ERROR << "EFC transaction failed";
+                EFC_Initialize();
+            }
+            taskYIELD();
+        }
+    };
+
 };
--- InternalFlash/src/InternalFlash.cpp
+++ InternalFlash/src/InternalFlash.cpp
@@ -1,37 +1,37 @@
-#include "InternalFlash/inc/InternalFlash.hpp"
+#include "lib/atsam-component-drivers/InternalFlash/inc/InternalFlash.hpp"
 
-InternalFlash::InternalFlash() {
-  EFC_Initialize();
+FlashDriver::FlashDriver() {
+    ;
 }
 
-EFC_ERROR InternalFlash::QuadWordWrite(uint32_t* data, uint32_t address) {
+EFC_ERROR FlashDriver::QuadWordWrite(FlashData* data, FlashAddress address) {
     EFC_QuadWordWrite(data, address);
 
-    while (EFC_IsBusy());
+    waitForResponse();
 
     return EFC_ErrorGet();
 }
 
-EFC_ERROR InternalFlash::PageWrite(uint32_t* data, uint32_t address) {
+EFC_ERROR FlashDriver::PageWrite(FlashData* data, FlashAddress address) {
     EFC_PageWrite(data, address);
 
-    while (EFC_IsBusy());
+    waitForResponse();
 
     return EFC_ErrorGet();
 }
 
-EFC_ERROR InternalFlash::Read(uint32_t* data, uint32_t length, uint32_t address) {
+EFC_ERROR FlashDriver::Read(FlashData* data, FlashEraseLength length, FlashAddress address) {
     EFC_Read(data, length, address);
 
-    while (EFC_IsBusy());
+    waitForResponse();
 
     return EFC_ErrorGet();
 }
 
-EFC_ERROR InternalFlash::SectorErase(uint32_t address) {
+EFC_ERROR FlashDriver::SectorErase(FlashAddress address) {
     EFC_SectorErase(address);
 
-    while (EFC_IsBusy());
+    waitForResponse();
 
     return EFC_ErrorGet();
 }
