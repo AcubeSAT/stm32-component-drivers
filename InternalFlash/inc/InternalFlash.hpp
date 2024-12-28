40e8a24686e5673933b3b760dc83053b68c2ffee
NPateras <npateras8@gmail.com>
2024-12-29 00:37:19 +0200

    make functions into templates, move the implementations to the header file and make small changes

--- InternalFlash/inc/InternalFlash.hpp
+++ InternalFlash/inc/InternalFlash.hpp
@@ -33,7 +33,7 @@ public:
         None,
         Timeout,
         AddressUnsafe,
-        AdressNotAlligned,
+        AdressNotAligned,
         InvalidCommand,
         RegionLocked,
         FlashError,
@@ -42,29 +42,34 @@ public:
     };
 
     /**
+     * Number of bits in a single byte, used for calculations.
+     */
+    static constexpr uint8_t numOfBitsinByte = 8;
+
+    /**
      * Wait period before an EFC transaction is skipped.
      */
-    static constexpr uint16_t TimeoutTicks = 1000;
+    static constexpr uint16_t timeoutTicks = 1000;
 
     /**
      * Size of a flash page in Bytes.
      */
-    static constexpr uint32_t FlashPageSize = 512;
+    static constexpr uint32_t flashPageSize = 512;
 
     /**
      * Size of 4 words (128 bits) in Bytes.
      */
-    static constexpr uint32_t QuadWordSize = 16;
+    static constexpr uint32_t quadWordSize = 16;
 
     /**
      * Starting adress of available Flash memory.
      */
-    static constexpr uint32_t StartAddress = 0x5F0000;
+    static constexpr uint32_t startAddress = 0x5F0000;
 
     /**
      * End limit of available Flash memory.
      */
-    static constexpr uint32_t EndAddress = 0x600000;
+    static constexpr uint32_t endAddress = 0x600000;
 
     /**
      * Constructor to create an instance of the class.
@@ -72,32 +77,88 @@ public:
     FlashDriver() = default;
 
     /**
-     * Write function that writes 128 bits. Only ‘0’ values can be programmed using Flash technology; ‘1’ is the erased value. In order to
+    * Templated write function for writing 128 bits (QuadWord). Only ‘0’ values can be programmed using Flash technology; ‘1’ is the erased value. In order to
      * program words in a page, the page must first be erased, as documented in Harmony Peripheral Libraries 2.39.3.
-     * @param data pointer to 128-bit data buffer.
+     * @param data Array containing the data to be written.
      * @param address FLASH address to be modified.
-     * @return member of the EFC_ERROR enum.
+     * @return member of the EFCError enum.
      */
-    static EFCError writeQuadWord(uint32_t data, FlashAddress_t address);
+    template <typename T, size_t N>
+    [[nodiscard]] static EFCError writeQuadWord( etl::array<T, N> data, FlashAddress_t address) {
+        static_assert((sizeof(T) * N) <= quadWordSize * numOfBitsinByte, "Data size exceeds 128 bits.");
+
+        if (not isAddressSafe(address)) {
+            return EFCError::AddressUnsafe;
+        }
+
+        const auto eraseResult = eraseSector(address);
+        if (eraseResult != EFCError::None) {
+            return eraseResult;
+        }
+
+        EFC_QuadWordWrite(static_cast<uint32_t *>(const_cast<T *>(data.data())), address);
+
+
+        if (waitForResponse() == EFCError::Timeout) {
+            return EFCError::Timeout;
+        }
+
+        return getEFCError();
+    }
 
     /**
-     * Write function that writes an entire page of 512 bytes. Only ‘0’ values can be programmed using Flash technology; ‘1’ is the erased value. In order to
-     * program words in a page, the page must first be erased, as documented in Harmony Peripheral Libraies 2.39.4.
-     * @param data pointer to data buffer of size equivalent to page size.
+    * Templated write function for writing a page. Only ‘0’ values can be programmed using Flash technology; ‘1’ is the erased value. In order to
+     * program words in a page, the page must first be erased, as documented in Harmony Peripheral Libraries 2.39.3.
+     * @param data Array containing the data to be written.
      * @param address FLASH address to be modified.
-     * @return member of the EFC_ERROR enum.
+     * @return member of the EFCError enum.
      */
-    static EFCError writePage(uint32_t data, FlashAddress_t address);
+    template <typename T, size_t N>
+    [[nodiscard]] static EFCError writePage( etl::array<T, N> data, FlashAddress_t address) {
+        static_assert((sizeof(T) * N) == flashPageSize * numOfBitsinByte, "Data size must match the page size.");
+
+        if (not isAddressSafe(address)) {
+            return EFCError::AddressUnsafe;
+        }
+
+        const auto eraseResult = eraseSector(address);
+        if (eraseResult != EFCError::None) {
+            return eraseResult;
+        }
+
+        EFC_PageWrite(static_cast<uint32_t *>(const_cast<T *>(data.data())), address);
+
+        if (waitForResponse() == EFCError::Timeout) {
+            return EFCError::Timeout;
+        }
+
+        return getEFCError();
+    }
 
     /**
-     * Reads length number of bytes from a given address in FLASH memory into
-     * the user buffer.
-     * @param data pointer to user data buffer.
-     * @param length number of bytes to read.
+     * Reads a specified length of bytes from a given address in FLASH memory into
+     * the user-provided buffer.
+     * @tparam T The type of data stored in the array (e.g., uint8_t or uint32_t).
+     * @tparam N The number of elements in the array.
+     * @param data A reference to an array of type T and size N to store the read data.
+     * @param length The number of bytes to read from the flash memory.
      * @param address FLASH address to be read from.
-     * @return member of the EFC_ERROR enum.
+     * @return A member of the EFCError enum indicating the result.
      */
-    static EFCError readFromMemory(uint32_t& data, FlashReadLength_t length, FlashAddress_t address);
+    template <typename T, size_t N>
+    [[nodiscard]] static EFCError readFromMemory(etl::array<T, N>& data,const FlashReadLength_t length, FlashAddress_t address) {
+        if (not isAddressSafe(address)) {
+            return EFCError::AddressUnsafe;
+        }
+
+        EFC_Read(static_cast<uint32_t*>(data.data()), length, address);
+
+        if (waitForResponse() == EFCError::Timeout) {
+            return EFCError::Timeout;
+        }
+
+        return getEFCError();
+}
 
 private:
     /**
@@ -116,7 +177,7 @@ private:
      * @return True if the address is within the limits defined.
      */
     static bool isAddressSafe(FlashAddress_t address) {
-        return address >= StartAddress && address < EndAddress;
+        return address >= startAddress && address < endAddress;
     }
 
     /**
@@ -165,7 +226,7 @@ private:
     static EFCError waitForResponse() {
         auto start = xTaskGetTickCount();
         while (EFC_IsBusy()) {
-            if (xTaskGetTickCount() - start > TimeoutTicks) {
+            if (xTaskGetTickCount() - start > timeoutTicks) {
                 LOG_ERROR << "EFC transaction failed";
                 EFC_Initialize();
              return EFCError::Timeout;
