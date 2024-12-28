d5f0cf1d5c2cdde1a3e54192b6351ad28b435f33
athatheo <athatheoc@gmail.com>
2024-12-28 23:18:27 +0000

    Update 3 files
    - /MRAM/inc/MR4A08BUYS45.hpp
    - /MRAM/inc/MRAMTestData.hpp
    - /MRAM/src/MR4A08BUYS45.cpp

--- MRAM/inc/MR4A08BUYS45.hpp
+++ MRAM/inc/MR4A08BUYS45.hpp
@@ -1,6 +1,14 @@
 #pragma once
 
 #include "SMC.hpp"
+#include "etl/span.h"
+enum class MRAM_Errno : uint8_t{
+    MRAM_NONE = 0,
+    MRAM_TIMEOUT = 1,
+    MRAM_ADDRESS_OUT_OF_BOUNDS = 2,
+    MRAM_READY= 3,
+    MRAM_NOT_READY = 4
+};
 
 class MRAM : public SMC {
 private:
@@ -9,12 +17,25 @@ private:
      * The device can store data from address 0 to 2^21-1.
      * Primarily used for error detection and loop limits.
      */
-    static inline constexpr uint32_t TotalNumberOfBytes = 0x200000;
+
+    static constexpr uint8_t customIDSize = 4;
+    static constexpr uint8_t customID[customIDSize] = {0xDE, 0xAD, 0xBE, 0xEF};
+
+    static constexpr uint32_t maxAllowedAddress   = 0x1FFFFF;                        // 0x200000-1
+    static constexpr uint32_t customMRAMIDAddress = maxAllowedAddress-customIDSize;  // Custom ID start Address
+    static constexpr uint32_t maxWriteableAddress = customMRAMIDAddress-1;           // Max allowed address for R/W data
 
     /**
-     * A variable to keep track of the total allocated memory and help in inserting new data.
+     * Nominally used only once to write the custom ID address
+     * on the predefined addresses
      */
-    uint32_t nextUnallocatedMemoryAddress = 0;
+    void writeID(void);
+    /**
+     * @param idArray: The custom ID address
+     * Used to read and check the custom ID from the
+     * predefined addresses in the MRAM module
+     */
+    bool checkID(uint8_t* idArray);
 
 public:
     /**
@@ -23,39 +44,43 @@ public:
      */
     constexpr MRAM(ChipSelect chipSelect) : SMC(chipSelect) {}
 
+    // R/W operations
+
     /**
      * Basic 8-bit write that takes advantage of the \ref moduleBaseAddress and creates an abstraction
      * to receive as a parameter directly an MRAM address.
      * @param dataAddress MRAM address to write to.
      * @param data 8-bit data to write to the address.
      */
-    inline void mramWriteByte(uint32_t dataAddress, uint8_t data) {
-        smcWriteByte(moduleBaseAddress | dataAddress, data);
-    }
+    MRAM_Errno mramWriteByte(uint32_t  dataAddress, const uint8_t &data);
 
     /**
-     * Basic 8-bit write that takes advantage of the \ref moduleBaseAddress and creates an abstraction
+     * Basic 8-bit read that takes advantage of the \ref moduleBaseAddress and creates an abstraction
      * to receive as a parameter directly an MRAM address.
      * @param dataAddress MRAM address to write to.
      * @return 8-bit data saved in that address.
      */
-    inline uint8_t mramReadByte(uint32_t dataAddress) {
-        return smcReadByte(moduleBaseAddress | dataAddress);
-    }
+    MRAM_Errno mramReadByte(uint32_t dataAddress, uint8_t &data);
 
     /**
-     * @param dataAddress Base MRAM address to write to. Data will be written starting from dataAddress to
-     * dataAddress + sizeOfData - 1.
+     * @param startAddress Base MRAM address to write to. Data will be written starting from startAddress to
+     * startAddress + sizeOfData - 1.
      * @param data Array of bytes to write to the memory module.
      * @param sizeOfData Number of bytes to write.
      */
-    void mramWriteData(uint32_t dataAddress, uint8_t *data, uint32_t sizeOfData);
+    MRAM_Errno mramWriteData(uint32_t startAddress, etl::span<const uint8_t> &data);
 
     /**
-     * @param dataAddress Base MRAM address to read from. Data will be read starting from dataAddress to
-     * dataAddress + sizeOfData - 1.
+     * @param startAddress Base MRAM address to read from. Data will be read starting from startAddress to
+     * startAddress + sizeOfData - 1.
      * @param data Array of bytes to store the data read from the memory module.
      * @param sizeOfData Number of bytes to read.
      */
-    void mramReadData(uint32_t dataAddress, uint8_t *data, uint32_t sizeOfData);
+    MRAM_Errno mramReadData(uint32_t startAddress, etl::span<uint8_t> data);
+
+    // HealthChecks & Handlers
+    MRAM_Errno isMRAMAlive();
+
+    void errorHandler(MRAM_Errno error);
+
 };
--- MRAM/src/MR4A08BUYS45.cpp
+++ MRAM/src/MR4A08BUYS45.cpp
@@ -1,1 +1,98 @@
+#include <etl/span.h>
 #include "MR4A08BUYS45.hpp"
+
+MRAM_Errno MRAM::mramWriteByte(uint32_t dataAddress, const uint8_t &data) {
+    uint32_t writeAddress = moduleBaseAddress | dataAddress;
+    if( (dataAddress > maxWriteableAddress) || (writeAddress > moduleEndAddress) ){
+        return MRAM_Errno::MRAM_ADDRESS_OUT_OF_BOUNDS;
+    }
+    smcWriteByte(writeAddress, data);
+    return MRAM_Errno ::MRAM_NONE;
+}
+
+MRAM_Errno MRAM::mramReadByte(uint32_t dataAddress, uint8_t &data) {
+    uint32_t readAddress = moduleBaseAddress | dataAddress;
+    if( (dataAddress > maxAllowedAddress) || (readAddress > moduleEndAddress) ){
+        return MRAM_Errno::MRAM_ADDRESS_OUT_OF_BOUNDS;
+    }
+    data = smcReadByte(readAddress);
+    return MRAM_Errno ::MRAM_NONE;
+}
+
+MRAM_Errno MRAM::mramWriteData(uint32_t startAddress, etl::span<const uint8_t> &data){
+    MRAM_Errno  error = MRAM_Errno::MRAM_NONE;
+    for(size_t i=0;i<data.size();i++){
+        error = mramWriteByte((startAddress+i), data[i]);
+        if(error!=MRAM_Errno::MRAM_NONE){
+            return error;
+        }
+    }
+    return error;
+}
+
+MRAM_Errno MRAM::mramReadData(uint32_t startAddress, etl::span<uint8_t> data){
+    MRAM_Errno  error = MRAM_Errno::MRAM_NONE;
+    for(size_t i=0;i<data.size();i++){
+        error = mramReadByte((startAddress+i), data[i]);
+        if(error!=MRAM_Errno::MRAM_NONE){
+            return error;
+        }
+    }
+    return error;
+}
+
+void MRAM::writeID(void){
+    for(size_t i=0;i<customIDSize;i++){
+        smcWriteByte( (moduleBaseAddress | (customMRAMIDAddress+i)), customID[i] );
+    }
+}
+
+bool MRAM::checkID(uint8_t* idArray){
+    for(size_t i=0;i<customIDSize;i++){
+        if(idArray[i]!=customID[i]){
+            return false;
+        }
+    }
+    return true;
+}
+
+MRAM_Errno MRAM::isMRAMAlive(){
+    etl::array<uint8_t, customIDSize> readId{};
+    etl::span<uint8_t> readIDspan(readId.data(), readId.size());
+    MRAM_Errno error = MRAM_Errno ::MRAM_NONE;
+    error = mramReadData(customMRAMIDAddress, readId);
+    if(error!=MRAM_Errno::MRAM_NONE){
+        return error;
+    }
+    if(checkID(readId.data())){
+        return MRAM_Errno::MRAM_READY;
+    }
+    // Try writing the id Since it can be the first time booting the device
+    writeID();
+    error = mramReadData(customMRAMIDAddress, readId);
+    if(error!=MRAM_Errno::MRAM_NONE){
+        return error;
+    }
+    if(checkID(readId.data())){
+        return MRAM_Errno::MRAM_READY;
+    }
+    //Couldn't read after write attempt, MRAM is unresponsive
+    return MRAM_Errno ::MRAM_NOT_READY;
+}
+
+void MRAM::errorHandler(MRAM_Errno error){
+    switch (error) {
+        case MRAM_Errno::MRAM_TIMEOUT:
+
+            break;
+        case MRAM_Errno::MRAM_ADDRESS_OUT_OF_BOUNDS:
+
+            break;
+        case MRAM_Errno::MRAM_NOT_READY:
+
+            break;
+        default:
+
+            break;
+    }
+}
