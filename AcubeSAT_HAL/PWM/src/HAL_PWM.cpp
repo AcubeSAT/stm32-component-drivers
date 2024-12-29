6c7f1dc728a7d3e32b8a4b170c979e442f573af5
athatheo <athatheoc@gmail.com>
2024-12-29 13:33:48 +0000

    Update 3 files
    - /LCL/src/LCLPWM.cpp
    - /AcubeSAT_HAL/PWM/inc/HAL_PWM.hpp
    - /AcubeSAT_HAL/PWM/src/HAL_PWM.cpp

--- AcubeSAT_HAL/PWM/src/HAL_PWM.cpp
+++ AcubeSAT_HAL/PWM/src/HAL_PWM.cpp
@@ -9,22 +9,12 @@ namespace HAL_PWM {
         PWM0_ChannelsStart(channelMask);
     }
 
-    template<>
-    void PWM_ChannelsStart<PeripheralNumber::Peripheral_1>(PWM_CHANNEL_MASK channelMask) {
-        PWM1_ChannelsStart(channelMask);
-    }
-
     template<PeripheralNumber peripheral>
     void PWM_ChannelsStop(PWM_CHANNEL_MASK channelMask) {}
 
     template<>
     void PWM_ChannelsStop<PeripheralNumber::Peripheral_0>(PWM_CHANNEL_MASK channelMask) {
         PWM0_ChannelsStop(channelMask);
-    }
-
-    template<>
-    void PWM_ChannelsStop<PeripheralNumber::Peripheral_1>(PWM_CHANNEL_MASK channelMask) {
-        PWM1_ChannelsStop(channelMask);
     }
 
     template<PeripheralNumber peripheral>
@@ -35,22 +25,12 @@ namespace HAL_PWM {
         PWM0_ChannelDutySet(pwmChannel, dutyCycle);
     }
 
-    template<>
-    void PWM_ChannelDutySet<PeripheralNumber::Peripheral_1>(PWM_CHANNEL_NUM pwmChannel, uint16_t dutyCycle) {
-        PWM1_ChannelDutySet(pwmChannel, dutyCycle);
-    }
-
     template<PeripheralNumber peripheral>
     uint16_t PWM_ChannelPeriodGet(PWM_CHANNEL_NUM pwmChannel) {}
 
     template<>
     uint16_t PWM_ChannelPeriodGet<PeripheralNumber::Peripheral_0>(PWM_CHANNEL_NUM pwmChannel) {
         return PWM0_ChannelPeriodGet(pwmChannel);
-    }
-
-    template<>
-    uint16_t PWM_ChannelPeriodGet<PeripheralNumber::Peripheral_1>(PWM_CHANNEL_NUM pwmChannel) {
-        return PWM1_ChannelPeriodGet(pwmChannel);
     }
 
     template<PeripheralNumber peripheral>
@@ -61,8 +41,30 @@ namespace HAL_PWM {
         PWM0_ChannelPeriodSet(pwmChannel, period);
     }
 
+#ifdef HAL_PWM_1
+    template<>
+    void PWM_ChannelsStart<PeripheralNumber::Peripheral_1>(PWM_CHANNEL_MASK channelMask) {
+        PWM1_ChannelsStart(channelMask);
+    }
+
+    template<>
+    void PWM_ChannelsStop<PeripheralNumber::Peripheral_1>(PWM_CHANNEL_MASK channelMask) {
+        PWM1_ChannelsStop(channelMask);
+    }
+
+    template<>
+    void PWM_ChannelDutySet<PeripheralNumber::Peripheral_1>(PWM_CHANNEL_NUM pwmChannel, uint16_t dutyCycle) {
+        PWM1_ChannelDutySet(pwmChannel, dutyCycle);
+    }
+
+    template<>
+    uint16_t PWM_ChannelPeriodGet<PeripheralNumber::Peripheral_1>(PWM_CHANNEL_NUM pwmChannel) {
+        return PWM1_ChannelPeriodGet(pwmChannel);
+    }
+
     template<>
     void PWM_ChannelPeriodSet<PeripheralNumber::Peripheral_1>(PWM_CHANNEL_NUM pwmChannel, uint16_t period) {
         PWM1_ChannelPeriodSet(pwmChannel, period);
     }
+#endif
 }
