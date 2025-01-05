fa0ad8eb4279fe3c17d13b8c010d2d58bec60708
athatheo <athatheoc@gmail.com>
2025-01-05 16:25:21 +0000

    Apply 1 suggestion(s) to 1 file(s)
    Co-authored-by: ggkogkou <ggkogkou@ece.auth.gr>

--- MRAM/inc/MRAMTestData.hpp
+++ MRAM/inc/MRAMTestData.hpp
@@ -2,7 +2,7 @@
 #include <etl/to_string.h>
 #include <etl/String.hpp>
 
-etl::array<uint8_t, 200> mram_test_data = {
+etl::array<uint8_t, 200> mramTestDatadata = {
     76, 111, 114, 101, 109, 32, 105, 112, 115, 117,
     109, 32, 100, 111, 108, 111, 114, 32, 115, 105,
     116, 32, 97, 109, 101, 116, 44, 32, 99, 111,
