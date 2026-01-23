8a40da5bd8f651d6a98d6a8c8d5f0b880925d6eb
Michail Tsialis <michalistsialis@gmail.com>
2026-01-23 17:59:27 +0200

    Add functions for complete I2C communication

---
+++ LM75/inc/LM75.hpp
@@ -0,0 +1,39 @@
+#pragma once
+#include <cstdint>
+#include <HAL_I2C.hpp>
+#include <etl/span.h>
+
+class LM75Sensor{
+public:
+
+    enum class Error {
+        /**
+         * The sensor could not read any data at this time
+         */
+        NO_DATA_AVAILABLE,
+        /**
+         * Sensor catastrophic failure, reset required
+         */
+        HARDWARE_FAULT,
+        UNKNOWN_ERROR
+    };
+
+    LM75Sensor();
+
+    Error write(etl::span<uint8_t> i2cData);
+    Error read(etl::span<uint8_t> buf);
+    void updateTemp();
+    void logTemp();
+private:
+    constexpr static uint8_t Lm75Addr = 0x48;
+    constexpr static uint8_t  Lm75Reg = 0x00;
+    uint16_t tempRead;
+    float temp;
+    Error lastError;
+    Error switchErrorWrite(HAL_I2C::I2CError error);
+    Error switchErrorRead(HAL_I2C::I2CError error);
+
+};
+
+
+
