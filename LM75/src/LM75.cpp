9e67063eab9d84815e762c00966df2c7ec1dc45a
Michail Tsialis <michalistsialis@gmail.com>
2026-01-23 17:59:27 +0200

    Add functions for complete I2C communication

--- LM75/src/LM75.cpp
+++ LM75/src/LM75.cpp
@@ -1,17 +1,13 @@
-#include "LM75.h"
+#include "LM75.hpp"
 #include <Logger.hpp>
 
 LM75Sensor::LM75Sensor() {
 };
 
-I2C1_Initialize();
 void LM75Sensor::updateTemp() {
-    if (!I2Cx_Read(LM45_ADDR, &tempRead, 1)){
-        this->lastError = Error::HARDWARE_FAULT;
-    }
-    if (!I2Cx_Read(LM45_ADDR, &tempRead, 2)){
-        this->lastError = Error::NO_DATA_AVAILABLE;
-    }
+    etl::span<uint8_t, 4> buf;
+    write(buf);
+    read(buf);
 }
 
 
@@ -19,3 +15,26 @@ void LM75Sensor::logTemp() {
     LOG_INFO << "The temperature is" << temp << "degC";
 }
 
+LM75Sensor::Error LM75Sensor::read(etl::span<uint8_t> buf) {
+    //placeholder
+    return switchErrorRead(HAL_I2C::readRegister<HAL_I2C::PeripheralNumber::TWIHS0>(Lm75Addr, buf));
+};
+
+LM75Sensor::Error LM75Sensor::write(etl::span<uint8_t> i2cData) {
+    //placeholder
+    return switchErrorWrite(HAL_I2C::writeRegister<HAL_I2C::PeripheralNumber::TWIHS0>(Lm75Addr, i2cData));
+};
+
+LM75Sensor::Error switchErrorRead(HAL_I2C::I2CError error) {
+    switch (error) {
+
+    default:return LM75Sensor::Error::UNKNOWN_ERROR;
+    }
+}
+
+LM75Sensor::Error switchErrorWrite(HAL_I2C::I2CError error) {
+    switch (error) {
+
+    default:return LM75Sensor::Error::UNKNOWN_ERROR;
+    }
+}
