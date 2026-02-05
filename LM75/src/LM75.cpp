f3770794f8109946a37f52833e2d310553b88d45
Michail Tsialis <michalistsialis@gmail.com>
2026-02-05 19:28:49 +0200

    Implement read logic

--- LM75/src/LM75.cpp
+++ LM75/src/LM75.cpp
@@ -5,7 +5,7 @@ LM75Sensor::LM75Sensor() {
 };
 
 void LM75Sensor::updateTemp() {
-    etl::span<uint8_t, 4> buf;
+    etl::array<uint8_t, 4> buf = Lm75Reg;
     write(buf);
     read(buf);
     temp = parseTemperature(buf[0],buf[1]);
@@ -16,7 +16,7 @@ void LM75Sensor::logTemp() {
     LOG_INFO << "The temperature is" << temp << "degC";
 }
 
-LM75Sensor::Error LM75Sensor::read(etl::span<uint8_t> buf) {
+LM75Sensor::Error LM75Sensor::read(etl::span<uint8_t> buf[0]) {
     //placeholder
     return switchErrorRead(HAL_I2C::readRegister<HAL_I2C::PeripheralNumber::TWIHS0>(Lm75Addr, buf));
 };
@@ -43,6 +43,6 @@ LM75Sensor::Error switchErrorWrite(HAL_I2C::I2CError error) {
 float LM75Sensor::parseTemperature(uint8_t msb, uint8_t lsb)
 {
     int16_t raw = (msb << 8) | lsb;
-    raw >>= 7;              // remove unused bits
-    return raw * 0.5f;      // multiply by the value of LSB
+    raw >>= 7;
+    return raw * 0.5f;
 }
