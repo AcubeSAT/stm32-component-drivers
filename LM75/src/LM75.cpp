96f356ca627d66a00dbddbb37e3ea275a83c0285
Michail Tsialis <michalistsialis@gmail.com>
2026-03-05 17:29:15 +0200

    Add full name for temperature variable

--- LM75/src/LM75.cpp
+++ LM75/src/LM75.cpp
@@ -13,11 +13,11 @@ etl::expected<float,LM75Sensor::Error> LM75Sensor::getTemperature() {
     if (auto error = read(i2cData); error != Error::NONE) {
         return etl::unexpected(error);
     }
-    float temp = parseTemperature(i2cData[0], i2cData[1]);
-    if (temp < TempMin || temp > TempMax) {
+    float temperature = parseTemperature(i2cData[0], i2cData[1]);
+    if (temperature < TempMin || temperature > TempMax) {
         return etl::unexpected(Error::INVALID_READ);
     }
-    return temp;
+    return temperature;
 }
 
 
